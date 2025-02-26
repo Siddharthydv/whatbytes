da0aafa05bf3a0131da4802fb9396d2391dfd481
Siddharthydv <saumyaydv22@gmail.com>
2025-02-26 11:20:49 +0530

    first

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Bell, Menu, UserCircle } from 'lucide-react';
+import { Bell, UserCircle } from 'lucide-react';
 
 export default function AppBar() {
   return (
